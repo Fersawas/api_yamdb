a8c9594b80b02bc4da7c6ac83caa76d2ca059e75
Yaroslav <berbasov.ya.v@ya.ru>
2024-02-08 03:51:29 +0300

    debug commit

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,19 +4,21 @@ from rest_framework import routers
 from . import views
 
 from api.views import (
-    CategoryViewSet, GenreViewSet, TitleViewSet, UserViewSet, ReviewViewSet, CategoryViewSet,
-    CommentViewSet
+    CategoryViewSet, GenreViewSet, TitleViewSet, UserViewSet, ReviewViewSet,
+    CategoryViewSet, CommentViewSet
 )
 
 router = routers.SimpleRouter()
 
 router.register(r'api/v1/titles', TitleViewSet, basename='title')
 router.register(r'api/v1/users', UserViewSet, basename='users')
-router.register(r'api/v1/titles/(?P<title_pk>\d+)/reviews', ReviewViewSet, basename='review')
+router.register(r'api/v1/titles/(?P<title_pk>\d+)/reviews',
+                ReviewViewSet, basename='review')
 router.register(r'api/v1/categories', CategoryViewSet)
 router.register(r'api/v1/genres', GenreViewSet)
-router.register(r'api/v1/titles/(?P<title_pk>\d+)/reviews/(?P<review_pk>\d+)/comments',
-                CommentViewSet, basename='comment' )
+router.register(
+    r'api/v1/titles/(?P<title_pk>\d+)/reviews/(?P<review_pk>\d+)/comments',
+    CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
